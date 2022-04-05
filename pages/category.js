938484b5f64e050ec3a21666318fbe26c045265f
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2022-04-05 14:53:17 -0700

    added voice stuff

---
+++ pages/category.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import GenericButton from '../comps/GenericButton';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Heading = styled.Text`
+    font-size: 20px;
+`
+
+const Cont = styled.View`
+    flex:1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`
+
+export default function Categories({navigation}) {
+    return (
+        <Cont>
+          <Heading>Add Categories...</Heading>
+          <StatusBar style="auto" />
+          <GenericButton onButtonPress={()=>{navigation.navigate('Home')}} buttonTitle="Home"/>
+          <GenericButton onButtonPress={()=>{navigation.navigate('Writing')}} buttonTitle="Writing"/>
+        </Cont>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import Voice from '@react-native-community/voice';
+import GenericButton from '../comps/GenericButton';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Home = ({navigation}) => {
+
+    const Stack = createNativeStackNavigator();
+  
+    const [result, setResult] = useState('')
+    const [isLoading, setLoading] = useState(false)
+  
+    useEffect(() => {
+      Voice.onSpeechStart = onSpeechStartHandler;
+      Voice.onSpeechEnd = onSpeechEndHandler;
+      Voice.onSpeechResults = onSpeechResultsHandler;
+  
+      return () => {
+        Voice.destroy().then(Voice.removeAllListeners);
+      }
+    }, [])
+  
+    const onSpeechStartHandler = (e) => {
+      console.log("start handler==>>>", e)
+    }
+    const onSpeechEndHandler = (e) => {
+      setLoading(false)
+      console.log("stop handler", e)
+    }
+  
+    const onSpeechResultsHandler = (e) => {
+      let text = e.value[0]
+      setResult(text)
+      setTimeout(()=>{
+        navigation.navigate(text)
+      },2000)
+      console.log("speech result handler", e)
+    }
+  
+    const startRecording = async () => {
+      setLoading(true)
+      try {
+        await Voice.start('en-Us')
+      } catch (error) {
+        console.log("error raised", error)
+      }
+    }
+  
+    const stopRecording = async () => {
+      try {
+        await Voice.stop()
+      } catch (error) {
+        console.log("error raised", error)
+      }
+    }
+  
+  
+    return (
+      <View style={styles.container}>
+        <SafeAreaView>
+          <Text style={styles.headingText}>Speech Recoginition</Text>
+          <View style={styles.textInputStyle}>
+            <TextInput
+              value={result}
+              placeholder="your text"
+              style={{ flex: 1 }}
+              onChangeText={text => setResult(text)}
+            />
+            {isLoading ? <ActivityIndicator size="large" color="red" />
+  
+              :
+              
+              <TouchableOpacity
+                onPress={startRecording}
+              >
+                <Image
+                  source={{ uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png' }}
+                  style={{ width: 25, height: 25 }}
+                />
+              </TouchableOpacity>}
+          </View>
+  
+          <TouchableOpacity
+            style={{
+              alignSelf: 'center',
+              marginTop: 24,
+              backgroundColor: 'red',
+              padding: 8,
+              borderRadius: 4
+            }}
+            onPress={stopRecording}
+          >
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Stop</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+        <GenericButton onButtonPress={()=>{navigation.navigate('Home')}} buttonTitle="Home"/>
+        <GenericButton onButtonPress={()=>{navigation.navigate('Writing')}} buttonTitle="Writing"/>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24
+    },
+    headingText: {
+      alignSelf: 'center',
+      marginVertical: 26,
+      fontWeight: 'bold',
+      fontSize: 26
+    },
+    textInputStyle: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      height: 48,
+      borderRadius: 20,
+      paddingHorizontal: 16,
+      shadowOffset: { width: 0, height: 1 },
+      shadowRadius: 2,
+      elevation: 2,
+      shadowOpacity: 0.4
+    }
+  });
+  
+  export default Home;
